8d9055d357ad7507e866d023e3c9e39ec4575688
Lee-ChinCheng <leeeo1211@gmail.com>
2026-03-31 11:53:41 +0800

    update script

--- step1_arrange.py
+++ step1_arrange.py
@@ -7,8 +7,8 @@ from aliasLLM import ask_ollama
 # -----------------------
 # Setting
 # -----------------------
-#inp_folder = '/mono10-50aa'
-inp_folder = '/home/cclee/DBAASP/mono10-50aa' #testing
+inp_folder = './mono10-50aa'
+#inp_folder = '/home/cclee/DBAASP/mono10-50aa' #testing
 op_folder = './output_csv'
 
 
